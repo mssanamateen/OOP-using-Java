b3ee241bd7d078b5dd7d310ac8f0a1339db6f576
mssanamateen <mssanamateen@gmail.com>
2024-03-09 13:39:52 +0530

    armstrongnumber

---
+++ basics/ArmstrongImpl.java
@@ -0,0 +1,25 @@
+package basics;
+import java.util.*;
+public class ArmstrongImpl {
+    //armstrong number logic
+    public static void main(String[] ar){
+        int sum=0,n,orn;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number:");
+        n=sc.nextInt();
+        orn=n;
+        while(orn!=0){
+             int rem=orn%10;
+             sum+=rem*rem*rem;
+             orn/=10;
+
+        }
+        if(sum==n){
+            System.out.println("armstrong number");
+        }else{
+            System.out.println("not armstrong number");
+        }
+        sc.close();
+    }
+    
+}
