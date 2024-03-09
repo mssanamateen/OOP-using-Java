dbfd6fee222e1749558d1c38fc26db3b2b095b98
mssanamateen <mssanamateen@gmail.com>
2024-03-09 14:03:08 +0530

    datatypes
    Co-Authored-By: Mq930 <118762764+Mq930@users.noreply.github.com>
    Co-Authored-By: Sana <63395977+mssanamateen@users.noreply.github.com>

---
+++ basics/Datatype.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class Datatype {
+    public static void main(String[] pp){
+        int a=90;
+        String s="hi";
+        System.out.println("datatype of s is "+s.getClass().getSimpleName());
+        System.out.println("datatype of a is "+Integer.TYPE);
+    }
+}
